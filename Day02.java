275f8dc7b6532a586b21f78dd3173d560a979f22
jfrycu-dotcom <jfrycu@gmail.com>
2025-09-09 13:37:46 +0800

    Create Day02.java

---
+++ Day02.java
@@ -0,0 +1,17 @@
+public class JEFRI_DAYTWO{
+    public static void main(String[] args) {
+        //penggunaan println
+        System.out.println("Nama: M. Jefri");
+        System.out.println("Umur: 20 tahun");
+        System.out.println("Prodi: Informatika");
+        //penggunaan pmrint
+        System.out.print("Haii!! nama saya Jefri, ");
+        System.out.print("Umur 20 tahun kelas A Informatika");
+        //Penggunaan printf
+        System.out.printf("Nama:%s %nUmur:%d %nProdi%s", "M. Jefri",20,"Informatika");
+        System.out.println("");
+        
+    }
+    
+    
+}
