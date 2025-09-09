7c4dfc2d93d6d134beccf04e86608070aa0dd67a
jfrycu-dotcom <jfrycu@gmail.com>
2025-09-10 06:32:36 +0800

    Day03.java

---
+++ Day03.java
@@ -0,0 +1,19 @@
+public class JEFRI_DAYTRE {
+    public static void main(String[] args) {
+        
+       //Bismillah...
+       
+//        "Prinln" digunakan agar hasil yang sudah dicetak 
+//        otomatis langsung pindah ke baris baru.
+        System.out.println("Nama saya Muhamad Jefri");
+//        kalau penggunaan "print" aja ga pake "ln" maka hasil 
+//        yang sudah dicetak tidak pindah atau dia tetap dibaris yang sama
+        System.out.print("Nama dia juga Muhammad Jefri Al Bukhori");
+        System.out.println("");
+//        penggunaan "printf" sendiri digunakan untuk menampilkan output format tertentu
+        System.out.printf("Nama: %s %nUmur: %d %nTinggi: %.1f","i am Muhammad Jefri Al-insani", 20,161.5);
+        System.out.println("");
+    }
+    
+    
+}
