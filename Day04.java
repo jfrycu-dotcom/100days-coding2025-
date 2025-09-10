6bfc43d46701fb1554925909c99a0a741784b574
jfrycu-dotcom <jfrycu@gmail.com>
2025-09-11 00:04:29 +0800

    Day04.java

---
+++ Day04.java
@@ -0,0 +1,12 @@
+public class BangJeffDayForr{
+    public static void main(String[] args) {       
+       //Bismillah...
+        System.out.println("Nama\t: M Jefri \nKelas\t: A Informatika \nFakul\t: Teknik");
+//        escape squence "\n" digunakan ketika kita mau memakai 
+//        satu sout aja tapi mau dijadikan garis baru.
+//        sedangkan escape squence "\t" digunakan untuk tab atau 
+//        untuk merapihkan baris inputan yang berantakan. 
+    }
+    
+    
+}
