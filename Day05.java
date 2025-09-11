6802f6f319ffa74916409cdb39cda9aaa3fc1d14
jfrycu-dotcom <jfrycu@gmail.com>
2025-09-12 07:58:43 +0800

    Day05.java

---
+++ Day05.java
@@ -0,0 +1,15 @@
+public class BgJepDay5 {
+    public static void main(String[] args) {       
+       //Bismillah...
+        byte angka1 = 127;
+        short angka2 = 32700;
+        int angka3 = 210030509;
+        long angka4 = 1000000000l;
+        System.out.println("Tipe data byte: "+angka1);
+        System.out.println("Tipe data short: "+angka2);
+        System.out.println("Tipe data integer: "+angka3);
+        System.out.println("Tipe data long: "+angka4);
+    }
+    
+    
+}
