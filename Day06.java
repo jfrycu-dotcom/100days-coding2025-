ee514e2ec2e8dcf32f0bbce955dd65727b64e9c8
jfrycu-dotcom <jfrycu@gmail.com>
2025-09-13 00:00:14 +0800

    Day06.java

---
+++ Day06.java
@@ -0,0 +1,11 @@
+public class BgJeffDay6 {
+    public static void main(String[] args) {       
+       //Bismillah...
+        double tinggi = 165.5;
+        float km = 8.5f;
+        System.out.println("Tinggi saya: "+tinggi);
+        System.out.println("Saya lari sejauh "+km+" kilometer");
+    }
+    
+    
+}
