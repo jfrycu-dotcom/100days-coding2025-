58d403d87b6ae37f89de3917d4ebc1099525e110
jfrycu-dotcom <jfrycu@gmail.com>
2025-09-14 00:00:12 +0800

    Day07.java

---
+++ Day07.java
@@ -0,0 +1,8 @@
+public class BgjeffDay07{
+	public static void main(String [] args){
+	char kelas = 'A';
+  boolean ya = true;
+            System.out.println("Saya kelas "+ kelas+"Informatika");
+            System.out.println(ya);
+        }
+}
