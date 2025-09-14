4a3f95049327d2a34fe64078e1396d952b524141
jfrycu-dotcom <jfrycu@gmail.com>
2025-09-15 00:01:42 +0800

    Day08.java

---
+++ Day08.java
@@ -0,0 +1,14 @@
+public class BgJepDay8 {  
+    public static void main(String[] args) {
+//        Bismillah....
+//        String bisa dipakai untuk teks maupun angka 
+//        selama masih di dalam tanda petik dua.
+        String nama = "M Jefri";
+        String nim = "D0225347";
+        String prodi = "Iformatika";
+        System.out.println("Nama saya\t: "+nama);
+        System.out.println("NIM saya\t: "+nim);
+        System.out.println("Prodi saya\t: "+prodi);
+    }
+    
+}
