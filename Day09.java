4d7f2eb86b5171091a4599b91b4f1c9232dd4d0f
jfrycu-dotcom <jfrycu@gmail.com>
2025-09-16 00:04:56 +0800

    Day09.java

---
+++ Day09.java
@@ -0,0 +1,23 @@
+public class BgJepDay9 {  
+    public static void main(String[] args) {
+        //Deklarasi
+        String nama;
+        int umur ;
+        double tinggi;
+        
+        //Inisialisasi
+        nama = "M Rizaldi";
+        umur = 17;
+        tinggi = 161.0;
+        
+        //Update variabel
+        nama = "M. Jefri";
+        umur = 20;
+        tinggi = 161.5;
+        System.out.println("Nama saya: "+nama);
+        System.out.println("Umur saya: "+umur);
+        System.out.println("Tinggi saya: "+tinggi);
+        
+    }
+    
+}
