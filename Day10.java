e13b7ecdf74890da6bbefbb48dee5f0329bb1585
jfrycu-dotcom <jfrycu@gmail.com>
2025-09-17 19:22:48 +0800

    Day10.java

---
+++ Day10.java
@@ -0,0 +1,14 @@
+public class BgJepDay10 {  
+    public static void main(String[] args) {
+        final String calon = "Chelsea";
+        final byte umurc = 18;
+        final int tinggi = 150;
+        final double bb = 54.5;
+        System.out.println("Nama calon: "+calon);
+        System.out.println("Umur calon: "+umurc);
+        System.out.println("Tinggi calon: "+tinggi);
+        System.out.println("Berat badan calon: "+bb);
+        
+    }
+    
+}
