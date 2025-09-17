8fd0290dc5881dc6a08aae925a374b94be0353ef
jfrycu-dotcom <jfrycu@gmail.com>
2025-09-18 00:04:11 +0800

    Day11.java

---
+++ Day11.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class BgJepDay11 {  
+    public static void main(String[] args) {
+        Scanner ah = new Scanner(System.in);
+        System.out.print("Masukkan kredit: ");
+        String h = ah .nextLine();
+        
+    }
+    
+}
