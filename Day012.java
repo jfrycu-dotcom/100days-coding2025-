f877adf8b3592a9c4471b2af3dc0600cceef6ca6
jfrycu-dotcom <jfrycu@gmail.com>
2025-09-19 22:09:46 +0800

    Day012.java

--- Day012.java
+++ Day012.java
@@ -1,23 +1,23 @@
 import java.util.Scanner;
 public class Ai_soalJeff {
     public static void main(String[] args) {
+
 //DAY 12 BROK
         
         //Scanner digunakan, supaya bisaki kasi masuk kayak 
         //teks atau angka di outputnya
         Scanner in = new Scanner(System.in);
         //String digunakan untuk teks aja
-        System.out.print("Masukkan nama: "); 
+        System.out.print("Masukkan nama  : "); 
         String nama = in.nextLine();
         //byte digunakan untuk angka, tapi angka yang ditampungnya hanya sedikit
-        System.out.print("Masukkan umur: ");
-        byte umur = in.nextByte();
+        System.out.print("Masukkan umur  : ");
+        int umur = in.nextInt();
         System.out.print("Masukkan Tinggi: ");
         double tinggi = in.nextDouble();
         System.out.println();
-        System.out.println("Nama saya adalah = "+nama);
-        System.out.print("Umur saya adalah = "+umur+" tahun");
-        System.out.printf("\nTinggi saya adalah = "+tinggi+" sentimeter\n");     
+        System.out.printf("Nama saya \t= %s \nUmur saya \t= %d tahun\nTinggi saya\t= %.1f cm\n",nama,umur,tinggi);
+                
     }
     
 }
