ec52d3db7645cd3a2dfd23adf9fa174d00f02b7a
jfrycu-dotcom <jfrycu@gmail.com>
2025-09-21 00:04:12 +0800

    Day14.java

---
+++ Day14.java
@@ -0,0 +1,8 @@
+public class BgJefDay14 {  
+    public static void main(String[] args) {
+        String nama = "100";
+        int gua = Integer.parseInt(nama);
+        System.out.println(gua);
+    }
+    
+}
