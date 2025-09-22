fd14d5124f79a34b8f79aaad0964b0680a383286
jfrycu-dotcom <jfrycu@gmail.com>
2025-09-23 07:27:47 +0800

    Day16.java

---
+++ Day16.java
@@ -0,0 +1,19 @@
+package dari_senior;
+
+import java.util.Scanner;
+public class nonSc {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Masukkan angka 1 : ");
+        int angka1 = in.nextInt();
+        System.out.print("Masukkan angka 2 : ");
+        int angka2 = in.nextInt();
+        System.out.println();
+        System.out.println("Hasil perkalian : "+(angka1*angka2));
+        System.out.println("Hasil pembagian : "+(angka1/angka2));
+                
+        
+        
+    }
+    
+}
