4939b8e302d23e72496d423c8a3367162fbb7dda
JEFF BROK <jfrycu@gmail.com>
2025-09-26 01:08:53 +0800

    Day19.java

---
+++ Day19.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Day_19 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Masukkan angka: ");
+        double a = in.nextDouble();
+        int b = (int)a;
+        System.out.println("Hasil konversinya adalah: "+b);
+    }
+}
