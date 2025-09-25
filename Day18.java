c746f8f41bfb3136ecbe572faccbbc49432271ff
JEFF BROK <jfrycu@gmail.com>
2025-09-25 13:51:27 +0800

    Day18.java

---
+++ Day18.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Day_18 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Masukkan angka: ");
+        byte a = in.nextByte();
+        short b = a;
+        System.out.println("Hasil konversinya adalah: "+b);
+    }
+}
