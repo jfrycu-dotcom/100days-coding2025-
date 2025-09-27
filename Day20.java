c875bdb74888aa456c7762a6195423e2b6204f2a
JEFF BROK <jfrycu@gmail.com>
2025-09-27 14:42:16 +0800

    Day20.java

---
+++ Day20.java
@@ -0,0 +1,11 @@
+package dari_senior;
+
+import java.util.Scanner;
+public class Day20 {
+    public static void main(String[] args) {
+        //Mengubah tipe data primitif menjadi String
+        int pin = 11112006;
+        String pin1 = Integer.toString(pin);
+        System.out.println("Hasil = "+pin1);
+    }
+}
