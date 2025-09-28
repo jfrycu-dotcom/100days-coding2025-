bb6063a0e87bcc895715822c669f5f2183db1ff4
JEFF BROK <jfrycu@gmail.com>
2025-09-28 12:42:56 +0800

    Day21.java

---
+++ Day21.java
@@ -0,0 +1,14 @@
+package belajarcoding;
+public class Aritmatika_jefff_Day21 {
+    public static void main(String[] args) {
+       
+        long x = 200;
+        int y = 100;
+        x=(short) (x+y); //jadi 300
+        y=(short) (x-y); //nilai y jadi 200
+        x=(short) (x-y); //lalu nilai x jadi 100
+        System.out.println("nilai x: "+x);
+        System.out.println("nilai y: "+y);
+    }
+    
+}
