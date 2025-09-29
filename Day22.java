9270b25ea86359de6cf5ae14220edf76e464a680
JEFF BROK <jfrycu@gmail.com>
2025-09-29 19:59:32 +0800

    Day22.java

---
+++ Day22.java
@@ -0,0 +1,21 @@
+package belajarcoding;
+
+import java.util.Scanner;
+
+public class Aritmatika_jefff {
+    public static void main(String[] args) {
+        
+        //DAY 22, mencari luas persegi : sisi x sis
+        Scanner in = new Scanner(System.in);
+        System.out.print("Masukkan sisi: ");
+        int sisi = in.nextInt();
+        long luas = sisi*sisi;
+        System.out.println("Sisinya adalah "+sisi+" cm");
+        System.out.println("Jadi luas persegi adalah "+luas+" cm");
+        
+        
+        
+        
+    }
+    
+}
