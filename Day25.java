db65b3720d8d4666e34359f35c9258fb27bd0aaa
JEFF BROK <jfrycu@gmail.com>
2025-10-02 00:44:29 +0800

    Day25.java

--- Day25.java
+++ Day25.java
@@ -1,21 +1,10 @@
-package latihan;
-
-import java.util.Scanner;
-
-public class elseIf {
+class Main {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        System.out.print("Masukkan angka1: ");
-        int angka1 = in.nextInt();
-        System.out.print("Masukkan angka2: ");
-        int angka2 = in.nextInt();
-        System.out.println();
-        angka1++;
-        angka2--;
-        System.out.println("Nilai increment: "+angka1);
-        System.out.println("Nilai decrement: "+angka2);
+        int a = 5;
         
-        
+        a++;
+        System.out.println("Hasil increment: "+a);
+        a--;
+        System.out.println("Hasil Decrement: "+a);
     }
- 
 }
