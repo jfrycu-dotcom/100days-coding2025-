253ee008f3a0692849c0abb435d3dd79cb53ba58
JEFF BROK <jfrycu@gmail.com>
2025-10-05 23:21:10 +0800

    Day28.java

--- Day28.java
+++ Day28.java
@@ -4,9 +4,9 @@ public class BelajarCoding {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.print("nama1 : ");
-        String nama1 = in.nextLine();
+        int nama1 = in.nextInt();
         System.out.print("nama2 : ");
-        String nama2 = in.nextLine();
+        int nama2 = in.nextInt();
         System.out.println("anu "+(nama1==nama2));
         System.out.println("anu "+(nama1!=nama2));
     }
