07b2becfc7bd19211f9cb8f20967431cda1f27db
JEFF BROK <jfrycu@gmail.com>
2025-10-17 00:08:53 +0800

    Day40.java

---
+++ Day40.java
@@ -0,0 +1,35 @@
+package main;
+
+import java.util.Scanner;
+
+public class kalkulator {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Masukkan angka pertama: ");
+        double a = in.nextDouble();
+        in.nextLine();
+        System.out.print("Masukkan operator(+,-,/,*,%): ");
+        String op = in.nextLine();
+        System.out.print("Masukkan angka kedua: ");
+        double b = in.nextDouble();
+        
+        double c = 0;
+        if(op.equals("+")){
+            c = a+b;
+            System.out.printf("Hasil penjumlahan: %.0f ",c);
+        }else if (op.equals("-")) {
+            c = a-b;
+            System.out.printf("Hasil pengurangan: %.0f",c);
+        }else if (op.equals("/")) {
+            c = a/b;
+            System.out.printf("Hasil pengurangan: %.0f",c);
+        }else if (op.equals("*")) {
+            c = a*b;
+            System.out.printf("Hasil perkalian: %.0f",c);
+        }else if (op.equals("%")) {
+            c = a%b;
+            System.out.printf("Hasil modulus: %.0f",c);
+        }
+        
+    }
+}
