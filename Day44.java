bc3ba36201ea6bfa8d0439940f060b23bac9d2c3
JEFF BROK <jfrycu@gmail.com>
2025-10-21 21:01:06 +0800

    Day44.java
    Welllll🤙🏻

--- Day44.java
+++ Day44.java
@@ -10,6 +10,7 @@ public class NewClass1 {
         
         int rata2 = (tugas+ujian)/2;
         System.out.println("Nilai rata-rata: "+rata2);
+        
         if (rata2>=85) {
             if (ujian>80) {
                 System.out.println("Predikat : A");
@@ -17,25 +18,28 @@ public class NewClass1 {
                 System.out.println("Predikat : B");
             }
         }else if (rata2>=75&&rata2<85) {
-            if (ujian>70&&ujian<75) {
+            if (ujian>70) {
                 System.out.println("Predikat : B");
             }else {
                 System.out.println("Predikat : C");
             }
         }else if (rata2>=65&&rata2<75) {
-            if (ujian>60&&ujian<70) {
+            if (ujian>60) {
                 System.out.println("Predikat : C");
             }else {
                 System.out.println("Predikat : D");
             }
         }else if (rata2>=50&&rata2<65) {
-            if (ujian>50&&ujian<60) {
+            if (ujian>50) {
                 System.out.println("Predikat : D");
             }else {
                 System.out.println("Predikat : E");
             }
         }else{
             System.out.println("Predikat : E");
-        }    
+        }
+        
+        
+        
     }
 }
