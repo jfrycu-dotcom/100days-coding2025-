c731cbf5c2f3a8b4a35a10b6b5e45cc99e6291ff
JEFF BROK <jfrycu@gmail.com>
2025-11-09 00:08:41 +0800

    Day63.java

---
+++ Day63.java
@@ -0,0 +1,15 @@
+package javaapplication24;
+import java.util.Scanner;
+public class soal03 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Masukkan nilai N: ");
+        int n = in.nextInt();
+        int total = 1;
+        for (int i = 1; i <= n; i++) {
+            System.out.print(i+" ");
+            total*=i;
+        }
+        System.out.println("\nTotal perkalian 1-"+n+" = "+total);
+    }
+}
