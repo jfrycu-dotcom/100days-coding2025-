a22351b0a4347b93e3521d71ebd5b9fa428f9e11
JEFF BROK <jfrycu@gmail.com>
2025-11-13 22:41:18 +0800

    Day67.java

---
+++ Day67.java
@@ -0,0 +1,9 @@
+package javaapplication24;
+public class soal03 {
+    public static void main(String[] args) {
+        System.out.println("<==Pola Horizontal==>");
+        for (int i = 0; i <= 5; i++) {
+            System.out.print("* ");
+        }System.out.println();
+    }
+}
