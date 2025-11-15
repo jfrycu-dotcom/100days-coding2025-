850ead787e5834c8a04de1685e907577e9267bcb
JEFF BROK <jfrycu@gmail.com>
2025-11-16 00:09:24 +0800

    Day70.java

---
+++ Day70.java
@@ -0,0 +1,12 @@
+package javaapplication24;
+public class soal03 {
+    public static void main(String[] args) {
+        System.out.println("<==POLA PERSEGI==>");
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 20; j++) {
+                System.out.print("* ");
+            }
+            System.out.println("*");
+        }
+    }
+}
