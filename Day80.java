7979389012eddb434b83b3ea15804d20a315eb86
JEFF BROK <jfrycu@gmail.com>
2025-11-26 00:13:41 +0800

    Day80.java

---
+++ Day80.java
@@ -0,0 +1,8 @@
+package anu1;
+import java.util.Arrays;
+public class tidak_main{
+    public static void main(String [] args){
+          int [] anuanu = {1,23,4,5,6,7,8,9,10};
+          System.out.println(Arrays.toString(anuanu));
+    }
+}
