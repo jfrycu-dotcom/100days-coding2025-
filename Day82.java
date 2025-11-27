eb954767d445edcad1d0fe8e15b3f79575ffe845
JEFF BROK <jfrycu@gmail.com>
2025-11-28 00:39:53 +0800

    Create Day82.java

---
+++ Day82.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner (System.in);
+        int [] cok = {1,2,3,4,5};
+        System.out.println("ISI ARRAY: ");
+        for (int yes = 0; yes < cok.length; yes++){
+            System.out.println(cok[yes]);
+        }
+    }
+}
