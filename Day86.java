9b6fa023f321e24f89ce693cf4f8480dbe0df1cb
JEFF BROK <jfrycu@gmail.com>
2025-12-02 00:33:46 +0800

    Create Day86.java

---
+++ Day86.java
@@ -0,0 +1,8 @@
+public class Day085_ {
+    public static void main(String[] args) {
+        int min = Integer.MAX_VALUE;
+        int a[] = {1,2,3};
+        for (int i = 0; i < a.length; i++) {if (a[i] < min ) m = a[i];}
+        System.out.println(min);
+    }
+}
