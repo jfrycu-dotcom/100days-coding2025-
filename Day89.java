0bfc349b218975788e627cf1dde496400d7567ce
JEFF BROK <jfrycu@gmail.com>
2025-12-05 01:36:14 +0800

    Create Day89.java

---
+++ Day89.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class day16 {
+    public static void main(String[] args) {
+        anu();
+    }static void anu(){
+        String heh = "ada yang seru nihhh, bocorin ga yaa @nyet";
+            System.out.println(heh+"\nhemm YTTA");
+    }
+}
