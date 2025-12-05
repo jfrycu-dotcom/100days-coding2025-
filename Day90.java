27ea9df83fb1b03d612e3599766fe4747f30e987
JEFF BROK <jfrycu@gmail.com>
2025-12-06 00:19:55 +0800

    Create Day90.java

---
+++ Day90.java
@@ -0,0 +1,11 @@
+
+import java.util.Arrays;
+import java.util.Scanner;
+public class FINAL_ICL_3 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println(luas(5,5));
+    }static double luas (double p, double l){
+        return p*l;
+    }
+}
