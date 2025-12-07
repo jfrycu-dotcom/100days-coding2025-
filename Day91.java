d6402d71399db2710ce4c492c5b0e452ea643deb
JEFF BROK <jfrycu@gmail.com>
2025-12-07 19:44:30 +0800

    Create Day91.java

---
+++ Day91.java
@@ -0,0 +1,8 @@
+package pkg09;
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(siJ(5, 10));
+    }static int siJ(int a, int b){
+        return a+b;
+    }
+}
