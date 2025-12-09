4757f71b0a397620f7cf3c6735c56e7f56d5e857
JEFF BROK <jfrycu@gmail.com>
2025-12-10 00:21:51 +0800

    Update Day94.java

--- Day94.java
+++ Day94.java
@@ -1,4 +1,4 @@
 public class main{
-    public static void main(String [] args)}kren();
+    public static void main(String [] args){kren();
     }static void kren(){System.out.println("KREN!!");}
 }
