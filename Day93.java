f8e36ac8ec7ca1b88649483408659fe46176b267
JEFF BROK <jfrycu@gmail.com>
2025-12-09 15:12:49 +0800

    Create Day93.java

---
+++ Day93.java
@@ -0,0 +1,13 @@
+
+import java.util.kren;
+
+public class NewClass {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        double a = in.nextDouble();
+        System.out.println(Math.ceil(a));
+        System.out.println(Math.floor(a));
+        System.out.println(Math.round(a));
+    }
+}
