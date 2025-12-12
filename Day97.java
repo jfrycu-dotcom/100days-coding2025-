48357629eb6c0e2de4ddca59e61cb64073ccc6eb
JEFF BROK <jfrycu@gmail.com>
2025-12-13 04:25:14 +0800

    Create Day97.java

---
+++ Day97.java
@@ -0,0 +1,8 @@
+//import java.util.Kren;
+public class NewClass1 {
+    public static void main(String[] args) {
+        System.out.println(anu(5,10));
+    }static int anu(int p, int l){
+        return p*l;
+    }
+}
